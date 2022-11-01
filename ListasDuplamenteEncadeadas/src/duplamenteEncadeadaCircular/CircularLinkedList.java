f268a17c53c04953e84463fdcf105bc76a82fecb
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-01 10:55:19 -0300

    circle

--- ListasDuplamenteEncadeadas/src/duplamenteEncadeadaCircular/CircularLinkedList.java
+++ ListasDuplamenteEncadeadas/src/duplamenteEncadeadaCircular/CircularLinkedList.java
@@ -1,5 +1,29 @@
 package duplamenteEncadeadaCircular;
 
-public class CircularLinkedList {
-    
+public class CircularLinkedList<T extends Comparable <T>> {
+    private DoublyNode<T> first;
+    private DoublyNode<T> last;
+    private int nodeNumber;
+
+    public boolean isEmpty(){
+        if(this.nodeNumber == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+    public void insertFirst(T value){
+        DoublyNode<T> newNode = new DoublyNode<>(value);  
+        
+        if(this.isEmpty()){
+            this.first = newNode;
+            this.last = newNode;
+            this.nodeNumber++;
+            this.last.setNext(this.first);  //garantindo circularidade da lista
+            this.first.setPrev(this.last);
+        }
+        
+    }
 }
