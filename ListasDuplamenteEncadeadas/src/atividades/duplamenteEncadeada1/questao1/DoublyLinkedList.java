914b9ed7b75658f051d0156086ecfdb6a9e3bfd2
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-06 17:00:31 -0300

    ss

--- ListasDuplamenteEncadeadas/src/atividades/duplamenteEncadeada1/questao1/DoublyLinkedList.java
+++ ListasDuplamenteEncadeadas/src/atividades/duplamenteEncadeada1/questao1/DoublyLinkedList.java
@@ -1,6 +1,6 @@
 package atividades.duplamenteEncadeada1.questao1;
 
-public class DoublyLinkedList<T> {
+public class DoublyLinkedList<T extends Comparable<T>> {
     private DoublyNode<T> first;
     private DoublyNode<T> last;
     private int nodeCounter;
@@ -12,5 +12,24 @@ public class DoublyLinkedList<T> {
         return false;
     }
 
-    
+    public DoublyNode<T> search(T value){
+        if(this.isEmpty()){
+            return null;
+        }
+        else if(value.compareTo(this.first.getInfo()) == 0){
+            return this.first;
+        }
+        else if(value.compareTo(this.last.getInfo()) == 0){
+            return this.last;
+        }
+        else{
+            DoublyNode<T> aux = this.first;
+            while(aux != null){
+                if(aux.getInfo().compareTo(value) == 0){
+                    return aux;
+                }
+            }  
+        }
+        return null;
+    }
 }
