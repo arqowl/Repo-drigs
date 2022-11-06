da6fb762c56c9e563d37014ce448d4c29921ca91
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-06 16:42:59 -0300

    e

--- ListasDuplamenteEncadeadas/src/atividades/duplamenteEncadeada1/questao1/Pessoa.java
+++ ListasDuplamenteEncadeadas/src/atividades/duplamenteEncadeada1/questao1/Pessoa.java
@@ -2,32 +2,41 @@ package atividades.duplamenteEncadeada1.questao1;
 
 public class Pessoa {
     private String nome;
-    private int rg;
+    private String rg;
 
-    public Pessoa(String nome, int rg) {
-        this.nome = nome;
-        this.rg = rg;
-    }
+
 
     public String getNome() {
         return nome;
     }
 
+
+
     public void setNome(String nome) {
         this.nome = nome;
     }
 
-    public int getRg() {
+
+
+    public String getRg() {
         return rg;
     }
 
-    public void setRg(int rg) {
+
+
+    public void setRg(String rg) {
         this.rg = rg;
     }
+
+
 
     @Override
     public String toString() {
         return "Pessoa [nome=" + nome + ", rg=" + rg + "]";
+    }
+
+    public int compareTo(Pessoa pessoa){
+        return this.rg.compareTo(pessoa.getRg());
     }
 
     
