604b9f43d50ec499933d7ec5bc0d030897828d0d
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-07 11:25:58 -0300

    deleted

--- ClassesConstructorsInheritance/src/classes/challenge1/Main.java
+++
@@ -1,8 +0,0 @@
-package classes.challenge1;
-
-public class Main {
-    BankAccount bobsAccount = new BankAccount("12345", 0.00, "Bob Brown", "myemail@bob.com", 
-    "(087) 123-4567");
-
-    //initiaalizing constructor with parameters.
-}
