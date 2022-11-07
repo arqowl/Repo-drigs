5ce8f984be3a7cff4dd416726713d136f49770a5
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-07 11:13:26 -0300

    main

--- InnerAstractInterface/src/exampleInterface/Main.java
+++ InnerAstractInterface/src/exampleInterface/Main.java
@@ -9,6 +9,11 @@ public class Main {
         timsPhone.callPhone(123456);
         timsPhone.answer();
 
+        timsPhone = new MobilePhone(24565);
+        //timsPhone.powerOn();
+        timsPhone.callPhone(24565);
+        timsPhone.answer();
+
         //you can define the actual type to be the type of the interface(ITelephone timsPhone = new Deskphone(123456);)
         //what we couldn´t do is (ITelephone timsPhone = new ITelephone(123456);)
     }
