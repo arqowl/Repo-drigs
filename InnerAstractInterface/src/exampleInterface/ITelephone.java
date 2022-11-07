bc679202b736bb16a3479e243e270cf66b62728b
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-07 10:38:49 -0300

    new

--- InnerAstractInterface/src/exampleInterface/ITelephone.java
+++ InnerAstractInterface/src/exampleInterface/ITelephone.java
@@ -1,11 +1,11 @@
 package exampleInterface;
 
 public interface ITelephone {
-    public void powerOn();
-    public void dial(int phoneNumber);
-    public void answer();
-    public boolean callPhone(int phoneNumber);
-    public boolean isRinging();
+    void powerOn();
+    void dial(int phoneNumber);
+    void answer();
+    boolean callPhone(int phoneNumber);
+    boolean isRinging();
 
     // for a class that is going to implement this interface, these are the methods that it has to implement
     // the acess modifiers are redundant for interface methods, because we are implementing the interface in a class,
