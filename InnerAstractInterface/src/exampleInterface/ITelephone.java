d5fefa029875bf75f0d93ca0f69001b84a294291
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-07 10:37:31 -0300

    yes

--- InnerAstractInterface/src/exampleInterface/ITelephone.java
+++ InnerAstractInterface/src/exampleInterface/ITelephone.java
@@ -8,4 +8,6 @@ public interface ITelephone {
     public boolean isRinging();
 
     // for a class that is going to implement this interface, these are the methods that it has to implement
+    // the acess modifiers are redundant for interface methods, because we are implementing the interface in a class,
+    // so, in other words, private or public in useless because you´re going to do that in the actual class.
 }
