007909cf0cabdb0da0c466c15b4c2322fca89dfc
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-07 10:18:03 -0300

    renamed

---
+++ InnerAstractInterface/src/exampleInterface/GearBox.java
@@ -0,0 +1,5 @@
+package exampleInterface;
+
+public class GearBox {
+    
+}
---
+++ InnerAstractInterface/src/exampleInterface/Main.java
@@ -0,0 +1,7 @@
+package exampleInterface;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
