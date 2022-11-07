96e76df3bd31072ce25d67a25187a1352b4ec42b
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-07 10:53:15 -0300

    aa

--- InnerAstractInterface/src/exampleInterface/Deskphone.java
+++ InnerAstractInterface/src/exampleInterface/Deskphone.java
@@ -8,27 +8,36 @@ public class Deskphone implements ITelephone {
 
     @Override
     public void answer() {
-        // TODO Auto-generated method stub
+        if(isRinging){
+            System.out.println("Answering the desk phone");
+            isRinging = false;
+        }
         
     }
     @Override
     public boolean callPhone(int phoneNumber) {
-        // TODO Auto-generated method stub
-        return false;
+        if(phoneNumber == myNumber){
+            isRinging = true;
+            System.out.println("Ring ring");
+        }else{
+            isRinging = false;
+        }
+        return isRinging;
+        
     }
     @Override
     public void dial(int phoneNumber) {
-        // TODO Auto-generated method stub
+        System.out.println("Now ringing "+phoneNumber +" on deskphone");
         
     }
     @Override
     public boolean isRinging() {
-        // TODO Auto-generated method stub
-        return false;
+        return isRinging;
     }
+    
     @Override
     public void powerOn() {
-        // TODO Auto-generated method stub
+        System.out.println("No action taken, desk phone does not have a power button");
         
     }
 
