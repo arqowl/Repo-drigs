dfaa0a777458c8f5504fa6098dbb303854964a7d
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-07 21:34:38 -0300

    oh yeah

--- ListasDuplamenteEncadeadas/src/atividades/duplamenteEncadeada1/questao1/DoublyLinkedList.java
+++ ListasDuplamenteEncadeadas/src/atividades/duplamenteEncadeada1/questao1/DoublyLinkedList.java
@@ -1,157 +1,153 @@
 package atividades.duplamenteEncadeada1.questao1;
 
 public class DoublyLinkedList<T extends Comparable<T>> {
+
     private DoublyNode<T> first;
     private DoublyNode<T> last;
-    private int nodeCounter;
+    private int nodeCount;
 
+    public boolean isEmpty() {
 
-    //INÍCIO QUESTÃO 1:
-    public boolean isEmpty(){
-        if(this.nodeCounter == 0){
+        if (this.nodeCount == 0) {
             return true;
+
         }
+
         return false;
+
     }
 
-    public DoublyNode<T> search(T value){
-        if(this.isEmpty()){
-            return null;
-        }
-        else if(value.compareTo(this.first.getInfo()) == 0){
-            return this.first;
-        }
-        else if(value.compareTo(this.last.getInfo()) == 0){
-            return this.last;
-        }
-        else{
-            DoublyNode<T> aux = this.first;
-            while(aux != null){
-                if(aux.getInfo().compareTo(value) == 0){
-                    return aux;
-                }
-            }  
-        }
-        return null;
-    }
+    public void insertFirst(T data) {
 
-    public void cadastrar(T value){
-        DoublyNode<T> newNode = new DoublyNode<>(value);
-        DoublyNode<T> result = this.search(value);
+        DoublyNode<T> newNode = new DoublyNode<>(data);
 
-        if(this.isEmpty()){
+        if (this.isEmpty()) {
+
             this.first = newNode;
             this.last = newNode;
-            this.nodeCounter = 1;
+            this.nodeCount++;
+
+        } else {
+
+            newNode.setNext(this.first);
+            this.first.setPrevious(newNode);
+            this.first = newNode;
+            this.nodeCount++;
         }
-        else{
-            if(result != null){
-                System.out.println("Usuário cadastrado!");
+
+    }
+
+    public void insertLast(T data) {
+
+        DoublyNode<T> newNode = new DoublyNode<>(data);
+
+        if (this.isEmpty()) {
+
+            this.first = newNode;
+            this.last = newNode;
+            this.nodeCount++;
+
+        } else {
+
+            this.last.setNext(newNode);
+            newNode.setPrevious(this.last);
+            this.last = newNode;
+            this.nodeCount++;
+
+        }
+
+    }
+
+    public void showData() {
+
+        DoublyNode<T> aux;
+
+        if (this.isEmpty()) {
+
+            System.out.println("List is empty");
+
+        } else {
+
+            aux = this.first;
+
+            while (aux != null) {
+
+                System.out.print(aux.getInfo() + " ");
+                aux = aux.getNext();
+
             }
-            else if(this.nodeCounter == 1){
-                this.first.setNext(newNode);
-                newNode.setPrevious();
-            }
-            else{
-                this.last.setNext(newNode);
-                this.last.setPrevious(this.last);
-                this.last = newNode;
-                this.nodeCounter++;
-            }
+
+            System.out.println(" ");
         }
     }
 
-    public void cancelar(){
-        if(this.isEmpty()){
-            System.out.println("Lista vazia!");
-        }
+    public void showDataLast() {
 
-        else if(this.nodeCounter == 1){
+        DoublyNode<T> aux;
+
+        if (this.isEmpty()) {
+
+            System.out.println("List is empty");
+
+        } else {
+
+            aux = this.last;
+
+            while (aux != null) {
+
+                System.out.print(aux.getInfo() + " ");
+                aux = aux.getPrevious();
+
+            }
+
+            System.out.println(" ");
+        }
+    }
+
+    public void removeFirst() {
+
+        if (this.isEmpty()) {
+
+            System.out.println("List is empty");
+
+        } else if (this.nodeCount == 1) {
+
             this.first = null;
             this.last = null;
-            nodeCounter = 0;
+            this.nodeCount--;
+
+        } else {
+
+            this.first = this.first.getNext();
+            this.first.setPrevious(null);
+            this.nodeCount--;
+
         }
-        else{
+
+    }
+
+    public void removeLast() {
+
+        if (this.isEmpty()) {
+
+            System.out.println("List is empty");
+
+        } else if (this.nodeCount == 1) {
+
+            this.first = null;
+            this.last = null;
+            this.nodeCount--;
+
+        } else {
+
             this.last = this.last.getPrevious();
             this.last.setNext(null);
-            this.nodeCounter--;
+            this.nodeCount--;
         }
+
     }
 
-    public void exibir(){
-        if(this.isEmpty()){
-            System.out.println("Vazia!");
-        }
-        else{
-            DoublyNode<T> aux = this.first;
-            while(aux != null){
-                System.out.println(aux.getInfo());
-                aux = aux.getNext();
-            }
-        }
-    }
-
-    public void verificarPresenca(T value){
-        DoublyNode<T> aux = this.first;
-
-        if(this.isEmpty()){
-            System.out.println("Lista vazia!");
-        }
-        else if(this.first.getInfo().compareTo(value) == 0){
-            System.out.println("Presente!");
-        }
-        else if(this.last.getInfo().compareTo(value) == 0){
-            System.out.println("Presente!");
-        }
-        else{
-            while(aux != null){
-                if(aux.getInfo().compareTo(value) == 0) {
-					System.out.println("Presente!");
-				}
-            }
-        System.out.println("Não está na lista!");
-        }
-    }
-
-    
-
-   
-     public void remover(T value){
-        DoublyNode<T> previous;
-        DoublyNode<T> after;
-
-        DoublyNode<T> result = this.search(value);
-
-        if(this.isEmpty()){
-            System.out.println("Lista vazia");
-        }
-        if(result == null){
-            System.out.println("Pessoa não encontrada!");
-        }
-        else{
-            if(value.compareTo(this.first.getInfo()) == 0){
-                this.first = this.first.getNext();
-                this.first.setPrevious(null);
-                this.nodeCounter--;
-
-            }else if(value.compareTo(this.last.getInfo()) == 0){
-                this.last = this.last.getPrevious();
-                this.last.setNext(null);
-                this.nodeCounter++;
-            }else{
-                previous = result.getPrevious();
-                after = result.getNext();
-
-                previous.setNext(after);
-                after.setPrevious(previous);   
-            }    
-        }
-        
-     }
-     //FIM QUESTÃO 1---------------------------------------------------------
-
-     //INICIO QUESTÃO 3(inserir em order crescente e aceita valores repetidos)
-     public void insertAscendingOrderRepeat(T data) {
+    public void insertCrescentOrder(T data) {
 
         DoublyNode<T> actualNode;
         DoublyNode<T> prevNode = null;
@@ -162,21 +158,21 @@ public class DoublyLinkedList<T extends Comparable<T>> {
 
             this.first = newNode;
             this.last = newNode;
-            this.nodeCounter++;
+            this.nodeCount++;
 
         } else if (data.compareTo(this.first.getInfo()) < 0) {
 
             newNode.setNext(this.first);
             this.first.setPrevious(newNode);
             this.first = newNode;
-            this.nodeCounter++;
+            this.nodeCount++;
 
         } else if (data.compareTo(this.last.getInfo()) > 0) {
 
             this.last.setNext(newNode);
             newNode.setPrevious(this.last);
             this.last = newNode;
-            this.nodeCounter++;
+            this.nodeCount++;
 
         } else if (data.compareTo(this.last.getInfo()) == 0) {
             System.out.println("Repetead value");
@@ -199,7 +195,7 @@ public class DoublyLinkedList<T extends Comparable<T>> {
                     actualNode.setPrevious(newNode);
                     newNode.setNext(actualNode);
                     newNode.setPrevious(prevNode);
-                    this.nodeCounter++;
+                    this.nodeCount++;
                     return;
 
                 } else {
@@ -210,52 +206,8 @@ public class DoublyLinkedList<T extends Comparable<T>> {
             }
         }
 
-
     }
-    public void insertDescendingOrderNoRepeat(T data){
-        DoublyNode<T> result = this.betterSearch(data);
-        DoublyNode<T> currentNode;
-        
-        DoublyNode<T> newNode = new DoublyNode<>(data);
-        if (this.isEmpty()) {
-            this.first = newNode;
-            this.last = newNode;
-            this.nodeCounter++;
-        }
-        else if(result == null){
-            if(data.compareTo(this.first.getInfo()) > 0){
-                newNode.setNext(newNode);
-                this.first.setPrevious(newNode);
-                this.first = newNode;
-            }
-            else if(data.compareTo(this.last.getInfo()) < 0){
-                this.last.setNext(newNode);
-                newNode.setPrevious(this.last);
-                this.last = newNode;
-            }
-            else{
-                currentNode = this.first;
-          
-                
-                if(data.compareTo(currentNode.getInfo()) > 0){
-                    
-                }
-                else{
-                     currentNode = currentNode.getNext();
-                }
-                
-            }
-        }
-        else{
-            System.out.println("Valor já existe!");
-        }
-    }
-
-    public void insertDescendingOrderRepeat(T data){
-
-    }
-
-    public DoublyNode<T> betterSearch (T value){  // busca sequencial melhorada
+    public DoublyNode<T> search (T value){  // busca sequencial melhorada
         DoublyNode<T> aux;
 
         
@@ -286,10 +238,39 @@ public class DoublyLinkedList<T extends Comparable<T>> {
         return null;
     }
 
+    public void remove(T value){
+        DoublyNode<T> result = this.search(value);
 
+        DoublyNode<T> before, after;
 
+        if(result == null){
+            System.out.println("Value has not been found");
+        }
+        else if(this.nodeCount == 1){  //lista com apenas 1 nó
+            this.first = null;
+            this.last = null;
+            this.nodeCount = 0;
+        }
+        else if(result == this.first){  //resultado igual a primeiro nó(remove primeiro)
+            this.first = this.first.getNext();
+            this.first.setPrevious(null);
+            this.nodeCount--;
+        }
+        else if(result == this.last){  // resultado igual a último nó(remove último)
+            this.last = this.last.getPrevious();
+            this.last.setNext(null);
+            this.nodeCount--;
+        }
+        else{  // remover no "meio"
+        before = result.getPrevious();
+        after = result.getNext();
 
+        before.setNext(after);
+        after.setPrevious(before);
+        this.nodeCount--;
 
-
+        }
+    }
     
+
 }
