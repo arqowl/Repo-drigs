f86ed37ba9f7752c3dbe6fc348c41f472875256a
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-09 12:19:39 -0300

    sim

--- ListasDuplamenteEncadeadas/src/duplamenteEncadeada/DoublyLinkedList.java
+++ ListasDuplamenteEncadeadas/src/duplamenteEncadeada/DoublyLinkedList.java
@@ -12,9 +12,67 @@ public class DoublyLinkedList<T extends Comparable<T>> {
     }
 
     public void insertFirst(T value){
-        DoublyNode<T> newNode = DoublyNode <> (value);
-        if(this.isEmpty()){
+        DoublyNode<T> newNode = new DoublyNode <> (value);
 
+        if(this.isEmpty()){
+            this.first = newNode;
+            this.last = newNode;
+            this.nodeCounter = 1;
+        }
+        else{
+            newNode.setNext(this.first);
+            this.first.setPrevious(newNode);
+            this.first = newNode;
+            this.nodeCounter++;
+        }
+    }
+
+    public void insertLast(T value){
+        DoublyNode<T> newNode = new DoublyNode <> (value);
+
+        if(this.isEmpty()){
+            this.first = newNode;
+            this.last = newNode;
+            this.nodeCounter = 1;
+        }
+        else{
+            this.last.setNext(newNode);
+            newNode.setPrevious(this.last);
+            this.last = newNode;
+            this.nodeCounter++;
+        }
+    }
+
+    public void deleteFirst(){
+
+        if(this.isEmpty()){
+            System.out.println("Lista vazia!");
+        }
+        else if(this.nodeCounter == 1){
+            this.first = null;
+            this.last = null;
+            this.nodeCounter = 0;
+        }
+        else{
+            this.first = this.first.getNext();
+            this.first.setPrevious(null);
+            this.nodeCounter--;
+        }
+    }
+
+    public void deleteLast(){
+        if(this.isEmpty()){
+            System.out.println("Lista vazia!");
+        }
+        else if(this.nodeCounter == 1){
+            this.first = null;
+            this.last = null;
+            this.nodeCounter = 0;
+        }
+        else{
+            this.last = this.last.getPrevious();
+            this.last.setNext(null);
+            this.nodeCounter--;
         }
     }
 
