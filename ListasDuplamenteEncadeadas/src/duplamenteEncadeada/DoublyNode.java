34dfdcc7c9158cb8ced1239e2da9a7317bd04dcb
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-09 11:51:38 -0300

    node

---
+++ ListasDuplamenteEncadeadas/src/duplamenteEncadeada/DoublyNode.java
@@ -0,0 +1,27 @@
+public class DoublyNode<T extends Comparable<T>> {
+    private DoublyNode<T> next;
+    private DoublyNode<T> previous;
+    private T data;
+    
+    public DoublyNode<T> getNext() {
+        return next;
+    }
+    public void setNext(DoublyNode<T> next) {
+        this.next = next;
+    }
+    public DoublyNode<T> getPrevious() {
+        return previous;
+    }
+    public void setPrevious(DoublyNode<T> previous) {
+        this.previous = previous;
+    }
+    public T getData() {
+        return data;
+    }
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    
+
+}
