7ed0f016f5ec2671b4f1ba2006e6e86c109efac9
Rodrigo Manzella <manzellateixeira@gmail.com>
2022-11-10 09:44:03 -0300

    ye

--- ListasDuplamenteEncadeadas/src/duplamenteEncadeada/Main.java
+++ ListasDuplamenteEncadeadas/src/duplamenteEncadeada/Main.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int escolha;
+
+        do{
+            exibir();
+            escolha = scan.nextInt(); scan.nextLine();
+        }while(escolha != 0);
+    }
+
+    public static void exibir(){
+        System.out.println("1 - inserir valores em ordem crescente com repetido");
+        System.out.println("2 - Mostrar a lista");
+    }
     
 }
